61c55c1e5c5878a5c0fbcd789355871de592a1da
Maharshi Shah <mshah@mobiquityinc.com>
2017-06-22 00:18:14 +0530

    Maven and TestNG integration

--- src/test/java/Homepage.java
+++ src/test/java/Homepage.java
@@ -1,16 +1,30 @@
-package com.tests;
+package test.java;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 public class Homepage {
-	public static void main(String[] args) throws InterruptedException {
+	WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
 		System.setProperty("webdriver.gecko.driver",
 				"/Users/maharshishah/Documents/Testing/libs/geckodriver");
 		System.setProperty("webdriver.chrome.driver",
 				"/Users/maharshishah/Documents/Testing/libs/chromedriver");
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void getHomePage() {
 		driver.get("http://automationpractice.com/");
+	}
+	
+	@AfterSuite
+	public void tearDown() {
 		driver.quit();
 	}
 }
